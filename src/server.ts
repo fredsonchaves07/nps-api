e4965aee1469ebffd846b048e4815b4b26ef50d2
Fredson Chaves <fredsonchaves07@gmail.com>
2021-02-22 22:08:51 -0300

    initial commig

---
+++ src/server.ts
@@ -0,0 +1,19 @@
+import express, { request, response } from 'express'
+
+const app = express()
+
+app.get('/', (request, response) => {
+    return response.json({
+        'message': 'Hello Word'
+    })
+})
+
+app.post('/', (request, response) => {
+    return response.json({
+        'message': 'Os dados foram salvos com sucesso'
+    })
+})
+
+app.listen(3333, () =>{
+    console.log('Server is running!')
+})
