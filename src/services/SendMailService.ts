b28a6d85f8f86c48dbc5c707c03084e85abaf0d4
Fredson Chaves <fredsonchaves07@gmail.com>
2021-03-03 19:23:51 -0300

    feat: implementation send email

---
+++ src/services/SendMailService.ts
@@ -0,0 +1,15 @@
+import nodemailer from 'nodemailer'
+
+class SendMailService {
+    
+    constructor(){
+        nodemailer.createTestAccount().then()
+    }
+
+    async execute(){
+
+        
+    }
+}
+
+export { SendMailService }
